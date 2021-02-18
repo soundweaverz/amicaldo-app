20ff6ff0e6d53d915ba785603192c172fae7f815
Jonas Nühnen <jonas.nuehnen@googlemail.com>
2021-02-18 18:47:01 +0100

    Add reference to crews

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     component: Users,
   },
   {
-    path: "/about",
+    path: "/crews",
     name: "Crews",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/views/Crews.vue
+++ src/views/Crews.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="crews">
+    <h1>Here should be a crew page.</h1>
   </div>
 </template>
