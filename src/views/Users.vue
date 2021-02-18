b66cbb3d1eae055f3cd9da6f010708c92c3ba519
Jonas Nühnen <jonas.nuehnen@googlemail.com>
2021-02-18 18:18:59 +0100

    Add basic table

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="home">
-    <p v-for="user in users" :key="user.ingameID">
-      {{ user.name }}
-    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Ingame-ID</th>
+          <th>Active</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <th>
+            <img
+              src="https://spielerfreunde.de/assets/images/pubgm/crews/189/keepPLO.jpg"
+              alt="Picture"
+            />
+          </th>
+          <th>{{ user.name }}</th>
+          <th>{{ user.ingameID ? user.ingameID : "No ID present." }}</th>
+          <th>{{ user.active ? "&check;" : "&#x274C;" }}</th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,7 +32,7 @@ import useUsers from "@/composables/useUsers";
 
 export default defineComponent({
   name: "Users",
-  setup(props) {
+  setup() {
     const { users, getUsers } = useUsers();
 
     return {
