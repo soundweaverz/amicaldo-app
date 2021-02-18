ab382bb3e2ceb6080a1f58fc3715d6b4388255a9
Jonas Nühnen <jonas.nuehnen@googlemail.com>
2021-02-18 18:01:36 +0100

    Delete default Vue home page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <p v-for="user in users" :key="user.ingameID">
       {{ user.name }}
     </p>
@@ -10,14 +8,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import useUsers from "@/composables/useUsers";
 
 export default defineComponent({
   name: "Users",
-  components: {
-    HelloWorld,
-  },
   setup(props) {
     const { users, getUsers } = useUsers();
 
